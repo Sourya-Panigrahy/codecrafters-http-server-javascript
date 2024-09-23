4b775203b7ddb12d21571a52276e2c55ba5f1d10
Sourya, Logesys In <sourya.panigrahy@logesys.in>
2024-09-23 16:28:59 +0530

    2nd stage

--- app/main.js
+++ app/main.js
@@ -1,18 +1,20 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
+// You can use print statements as follows for debugging; they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  console.log("Received a request.");
 
-    // Log the response being sent
-    console.log("Sent response: HTTP/1.1 200 OK");
-    
-    socket.end();
+  socket.on("error", (err) => {
+    console.error("Socket error:", err);
   });
+
+
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
+  socket.end();
 });
+
 
 server.listen(4221, "localhost");
